7643a84afa0a92f509e6c56176eca776aefe4580
AndyVale <andreavalentino.ricotti@gmail.com>
2024-08-14 12:03:42 +0200

    [UPDATED] argparse in extract_compress_files_python

--- extract_compress_files_python.py
+++ extract_compress_files_python.py
@@ -2,8 +2,10 @@ import os
 import gzip
 import sys
 import shutil
+import argparse
 
 def extract_files():
+    '''Extracts all .gz files in the current directory'''
     for root, dirs, files in os.walk("."):
         for file in files:
             if file.endswith(".gz"):
@@ -13,6 +15,7 @@ def extract_files():
                 os.remove(os.path.join(root, file))
 
 def compress_files():
+    '''Compresses all .vnnlib and .onnx files in the current directory'''
     for root, dirs, files in os.walk("."):
         for file in files:
             if file.endswith(".vnnlib") or file.endswith(".onnx"):
@@ -22,12 +25,40 @@ def compress_files():
                 os.remove(os.path.join(root, file))
 
 def main():
-    if sys.argv[1] == "compress" or sys.argv[1] == "c" or sys.argv[1] == "comp":
+    parser = argparse.ArgumentParser(
+    prog='extract_compress_files_python',
+    description=
+"""
+------------------------------------------------------------------------------------------------------
+This script will recursively extract .gz files and compress .vnnlib and .onnx files in the current directory.
+
+To extract .gz files, use the extract mode.
+To compress .vnnlib and .onnx files, use the compress mode.
+
+examples:
+    python extract_compress_files_python.py --mode extract
+    python extract_compress_files_python.py --mode compress
+
+or using the short versions:
+    python extract_compress_files_python.py -m e
+    python extract_compress_files_python.py -m c
+""",
+    epilog=
+"""Thank you for using the script! :)
+------------------------------------------------------------------------------------------------------""",
+    formatter_class=argparse.RawDescriptionHelpFormatter
+    )
+
+    parser.add_argument('-m', '--mode', type=str, help='Mode: compress or extract', required=True)
+    args = parser.parse_args()
+    if args.mode == "compress" or args.mode == "c":
         print("Compressing .vnnlib and .onnx files...")
         compress_files()
-    else:
+    elif args.mode == "extract" or args.mode == "e":
         print("Extracting .gz files...")
         extract_files()
+    else:
+        parser.print_help()
 
 if __name__ == "__main__":
     main()
